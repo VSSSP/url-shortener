9449e61dc25ac390697e6f10d1009fa09acb5be1
VSSSP <victorssspaula@live.com>
2024-08-13 22:30:01 -0300

    refactor: add email validation

--- src/application/services/auth.js
+++ src/application/services/auth.js
@@ -1,30 +1,52 @@
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs')
 const User = require('../../domain/models/user');
+const validator = require('validator');
 
 const register = async (email, password) => {
+    if (!validator.isEmail(email)) {
+        throw new Error('Invalid email format');
+    }
+
+    const existingUser = await User.findOne({
+        where: {
+            email
+        }
+    });
+    if (existingUser) {
+        throw new Error('Email already in use');
+    }
+
     const hashedPassword = await bcrypt.hash(password, 10);
     const user = await User.create({
         email,
         password: hashedPassword
     });
+
     return user;
 };
 
 const login = async (email, password) => {
+    if (!validator.isEmail(email)) {
+        throw new Error('Invalid email format');
+    }
+
     const user = await User.findOne({
         where: {
             email
         }
     });
+
     if (!user || !(await bcrypt.compare(password, user.password))) {
         throw new Error('Invalid credentials');
     }
+
     const token = jwt.sign({
         id: user.id
     }, process.env.JWT_SECRET, {
         expiresIn: '1h'
     });
+
     return {
         token
     };
