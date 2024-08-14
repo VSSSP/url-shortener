16dae6f88ddce99b2d64ca79635a63f732e7e685
VSSSP <victorssspaula@live.com>
2024-08-13 22:47:39 -0300

    feat: add swagger documentation ui

--- src/server.js
+++ src/server.js
@@ -3,14 +3,22 @@ const cors = require('cors');
 const {
     json
 } = require('body-parser');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const path = require('path');
 const authRoutes = require('./interfaces/authController');
 const urlRoutes = require('./interfaces/urlController');
 const sequelize = require('./infrastructure/database');
 require('dotenv').config();
 
 const app = express();
+
+const swaggerDocument = YAML.load(path.join(__dirname, 'docs', 'swagger.yaml'));
+
 app.use(cors());
 app.use(json());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use('/auth', authRoutes);
 app.use('/', urlRoutes);
